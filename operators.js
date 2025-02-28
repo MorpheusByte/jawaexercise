0f6fae0d05baa8e3f952dabcfa86fde4ded76b9b
MorpheusByte <tahasaidcakmak@icloud.com>
2025-02-28 20:43:06 +0300

    begin

---
+++ operators.js
@@ -0,0 +1,15 @@
+/* ---------------------------------- */
+//!             OPERATORS             */
+/* ---------------------------------- */
+//! Aritmatik Operatorler
+
+let a = 10
+let b = "2"
+
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a%b)
+console.log(a**b)
+
