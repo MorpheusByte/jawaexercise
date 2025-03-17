34f5e01731db238a337ecf99592cb93853a1f80f
MorpheusByte <tahasaidcakmak@icloud.com>
2025-03-17 21:53:42 +0300

    endlesson2

--- Arrays2/array.js
+++ Arrays2/array.js
@@ -8,4 +8,57 @@
 /* -------------------------------------------------------------------------- */
 // Bir arrayın kaç elemandan oluştuğunu verir
 
-const number = [3, 5, 2, "4", "bes", "dort", 3, 6, "bes", 5,56]
+const number = [3, 5, 2, "4", "bes", "dort", 3, 6, "bes", 5, 56, 1453];
+
+console.log(`Number arrayi ${number.length} eleman sayısına sahiptir`);
+
+/* -------------------------------------------------------------------------- */
+//!                                  INCLUDES()                                */
+/* -------------------------------------------------------------------------- */
+
+//* Bir elemanın dizide bulunup bulunmadığını true veya false olarak döndürür.
+
+console.log(number.includes(5)); // number dizisi 5 sayısını içeriyor mu? true
+console.log(number.includes(9)); // number dizisi 9 sayısını içeriyor mu? false
+
+// const userList = ["excalibur16", "cezalandırıcı28", "fişek44", "tozduman55", "crazyboy_56"]
+// const guest = prompt("Bir kullanıcı adı girin")
+// userList.includes(guest) ? console.log("KAyıtlı kullanıcı giriş yap") : console.log("Kayıt olabilirsiniz")
+
+/* -------------------------------------------------------------------------- */
+//!                          indexOf(), lastIndexOf()                         */
+/* -------------------------------------------------------------------------- */
+// Bir array içinde aradığımız elamanı ilk gördüğün indexin numarasını döndürür
+// İçinde bulamazsa (yoksa) -1 değeri döndürür
+
+if (number.indexOf("bes") < 0) {
+  console.log("Aranan öğe yok");
+} else {
+  console.log(`Aradığınız öğenin index numarası ${number.indexOf("bes")}`);
+
+  // lastIndexOf()
+  // sondan başlayarak bakar ilk gördüğü index numarası verir
+
+  console.log(number.lastIndexOf(5));
+}
+
+/* -------------------------------------------------------------------------- */
+//!                          join                                             */
+/* -------------------------------------------------------------------------- */
+//* Dizideki tüm elemanları belirtilen bir ayraç ile birleştirerek bir string döndürür.
+
+const statement = ["oku", "da", "adam" , "ol"]
+console.log(statement.join("💋"))
+
+console.log(statement)
+
+const newStr = statement.join("👴🏻")
+console.log(newStr)
+
+/* -------------------------------------------------------------------------- */
+//!                        slice()                                           */
+/* -------------------------------------------------------------------------- */
+// slice(start,end) 
+// end verilmezse onuna kadar olanı alır
+console.log(statement.slice(1,3))
+console.log(statement)
