a224f8c4d1fe015dace4a5c98afcd5368b236d94
MorpheusByte <tahasaidcakmak@icloud.com>
2025-03-24 21:55:32 +0300

    Object1

---
+++ Object/object.js
@@ -0,0 +1,13 @@
+/* -------------------------------------------------------------------------- */
+/*                                   OBJECT                                   */
+/* -------------------------------------------------------------------------- */
+
+//! Diziler sıralı bellek bölgeleridir ve sıralı bir şekilde ulaşılabilir. Ama daha karmaşık veriler için Object kullanılır
+
+//! Objectlerde key-value ( Property-value) değerlerini içerir. Bu o nesnenin özelliklerini belirtir.
+
+//! Herhangi bir veriye erişim için property (key) adı kullanılır.
+
+const isim = ["Ali", "Veli", "Sema"]
+const soyad = ["Yılmaz", "Çelik", "Taş"]
+
