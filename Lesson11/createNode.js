8f3d11bac0549307095c01406c9bac396721df93
MorpheusByte <tahasaidcakmak@icloud.com>
2025-04-07 21:01:34 +0300

    Lesson1End1

--- Lesson11/createNode.js
+++ Lesson11/createNode.js
@@ -25,7 +25,8 @@ react.before(newLi2, newLi)
 //? 1- element.attribute = "yeni deger"
 // *yazma
 newLi.className = "list hello red bold text-center  bg-primary text-warning"
-newLi.id = "new-li"
+newLi.id = "new-li";
+// newLi.className = "bg-warning"
 
 //* Okuma
 
@@ -39,3 +40,35 @@ newLi2.setAttribute("id","new-li2")
 
 console.log(newLi2.getAttribute("id"))
 console.log(newLi2.getAttribute("class"))
+
+//? 3) Yontem (classList)
+
+newLi.classList.add("border", "border-5", "border-dark")
+
+newLi.classList.remove("bold")
+
+console.log(newLi.classList.contains("bold"))
+console.log(newLi.classList.contains("list"))
+
+newLi.classList.toggle("border-dark")
+newLi.classList.toggle("border-dark")
+
+//* ------- YENI BIR ELEMENT OLUSTURMA (InnerHTML)------
+
+
+document.querySelector("main").innerHTML += 
+`
+<div>
+    <input type="text"/>
+    <button class="btn btn-danger" >Submit</button>
+</div>
+
+<div class="card" style="width: 12rem;">
+  <img src="https://cdn.pixabay.com/photo/2025/03/26/09/08/ai-generated-9494063_1280.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+`
