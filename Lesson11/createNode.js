d0e94efc2ea00aef6fc79fe90e74acae018f6ec1
MorpheusByte <tahasaidcakmak@icloud.com>
2025-04-07 20:33:31 +0300

    Class

--- Lesson11/createNode.js
+++ Lesson11/createNode.js
@@ -6,3 +6,13 @@ newLi.appendChild(textLi)
 
 document.querySelector("ul").appendChild(newLi)
 
+
+const newLi2 = document.createElement("li")
+newLi2.textContent = "C++"
+
+const react = document.querySelector(" ul li:nth-child(4)")
+
+// react.appendChild(newLi2)
+react.before(newLi2)
+react.after(newLi2, newLi)
+
