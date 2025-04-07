83bdc9ed9dd605d1006830636334b0558a6c4b95
MorpheusByte <tahasaidcakmak@icloud.com>
2025-04-07 21:58:51 +0300

    EventsLesson1End

--- Event/events.js
+++ Event/events.js
@@ -9,6 +9,8 @@ console.log("******Events******")
 const h2 = document.querySelector("h2")
 const body = document.querySelector("body")
 const myInput = document.getElementById("input")
+const ul = document.querySelector("ul")
+const addBtn = document.getElementById("btn")
 
 
 // Event Tanımlama
@@ -29,3 +31,27 @@ h2.onmouseout = () =>{
 body.onload= function(){
     myInput.focus()
 }
+
+//? DOMContentLoaded (metin v.b içerikler yüklenince tetiklenir. Resim, link gibi içeriklerin yüklenmesini beklemez)
+// document.addEventListener("DOMContentLoaded",(e)=>{
+//     console.log("çalıştı")
+// })
+
+addBtn.addEventListener("click", (e)=>{
+    // console.log(e.target)
+    // console.log(e.target.value)
+    // console.log(myInput.value)
+
+    if (!myInput.value) {
+        alert("lütfen boş bırakmayınız")
+        
+    }else{
+        const li = document.createElement("li")
+        li.textContent=myInput.value
+        ul.appendChild(li)
+        myInput.value = ""
+    }
+
+
+
+})
